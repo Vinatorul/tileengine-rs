52e35e25cc5f6aaa99f2fe4d542636911c11c957
Alexander <alexander@kuvaev.me>
2016-02-22 04:38:27 +0300

    Fixed tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,20 @@
 #[test]
 fn get_rect() {
-	use tile;
+    use tile;
     let tile = tile::Tile::new(1, 2, 3, 4);
-    assert_eq!(tile.rect(), [1, 2, 3, 4]);
+    assert_eq!(tile.rect(0, 0), [1, 2, 3, 4]);
+}
+
+#[test]
+fn get_rect_offset() {
+    use tile;
+    let tile = tile::Tile::new(1, 2, 3, 4);
+    assert_eq!(tile.rect(30, 100), [31, 102, 3, 4]);
+}
+
+#[test]
+fn get_rect_neg() {
+    use tile;
+    let tile = tile::Tile::new(1, 2, 3, 4);
+    assert_eq!(tile.rect(-100, -299), [-99, -297, 3, 4]);
 }
